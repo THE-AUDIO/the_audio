be067ae144cc61cea690df475ec5f51413d57820
THE-AUDIO <nandraina.dev22@gmail.com>
2026-02-05 12:41:43 +0300

    init header

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('the_audio');
+  protected readonly title = signal('THE Audio Nandraina');
 }
